57382b6e83886aaf28c27ab850af1fa4377a29ae
Daniel Oliveira <daniel.olive.dev@gmail.com>
2023-05-25 17:05:24 -0300

    Fiz o sistema funcionar nesse atualização, agora temos as telas de Status do calcula passando as informações de pois do calculo.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import powerImage from './assets/powered.png';
 import { GridItem } from './components/Griditem';
+import leftArrowImage from './assets/leftarrow.png';
 
-import { levels, calculateImc } from './helpers/imc';
+import { levels, calculateImc, Level } from './helpers/imc';
 
 
 const App = () => {
   const [heightField, setheightField] = useState<number>(0);
   const [weightField, setweightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
   const handleCalculateButton = () => {
     if (heightField && weightField) {
-
+      setToShow(calculateImc(heightField, weightField));
     } else {
       alert("Digite todos os campos.")
     }
-  }
 
+  }
+  const handleBackButton = () => {
+    setToShow(null);
+    setheightField(0);
+    setweightField(0);
+
+  }
   return (
     <div className={styles.main}>
       <header>
@@ -36,22 +44,35 @@ const App = () => {
             placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
             value={heightField > 0 ? heightField : ''}
             onChange={e => setheightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
           <input
             type="number"
             placeholder="Digite a seu peso. Ex: 75.5 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setweightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false} >Calcular</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, key) => (
-              <GridItem key={key} item={item} />
-            ))}
-          </div>
+          {!toShow && 
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          }
+          {toShow && 
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          }
+          
         </div>
       </div>
     </div>
--- src/components/Griditem/index.tsx
+++ src/components/Griditem/index.tsx
@@ -14,9 +14,12 @@ export const GridItem = ({ item }: Props) => {
                 {item.icon === 'up' && <img src={upImage} alt="" width="30" />}
                 {item.icon === 'down' && <img src={downImage} alt="" width="30" />}
             </div>
-            <div className={styles.gridTitle}>
-                {item.title}
-            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+
+            {item.yourImc &&
+                <div className={styles.yourImc}>Seu IMC é de {item.yourImc} kg/m²</div>
+            }
+
             <div className={styles.gridInfo}>
                 <>
                 IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
